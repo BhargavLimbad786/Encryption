b7589d8851629bd7ec9cd747460efffbded2ed9e
BhargavLimbad786 <bhargav@e6x.io>
2025-01-27 23:44:50 +0530

    added all requirment files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Read the contents of the README file
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="encyption",  # Name of your package
+    version="0.1.0",      # Initial version number
+    author="Bhargav Limbad",  # Your name
+    author_email="bhargav@e6x.io",  # Your email
+    description="A simple AES and RSA encryption package",  # Short description
+    long_description=long_description,  # Detailed description (from README)
+    long_description_content_type="text/markdown",  # Markdown format
+    url="https://github.com/BhargavLimbad786/Encryption",  # URL to your project repository
+    packages=find_packages(),
+    install_requires=["pycryptodome"],   # Automatically find packages in the current directory
+    classifiers=[  # A list of classifiers that help users find your package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Minimum Python version
+    install_requires=[  # List of dependencies (if any)
+        "pycryptodome",  # Example, if you use PyCryptodome
+    ],
+    test_suite='tests',  # Automatically find test files in the tests folder
+)
